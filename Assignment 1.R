c852a7f74c814a2e67228e8f8ed00ff2b25902b2
Ricebabyhaha <zwhu@ucdavis.edu>
2015-04-04 20:41:10 -0800

    Assignment 1

--- Assignment 1.R
+++ Assignment 1.R
@@ -1,10 +1,11 @@
 ##Step one: reading data into R##
+##Get the brief idea of the data
 setwd("d:\\00 Davis\\03 2015 Spring Quarter\\Stat 242\\Assignment 1\\stat242_2015\\Assignment1\\data")
 list.dirs() #check the current directory
 dir()
 list.files()
-readLines("men10Mile_1999")
-class(men10Mile_1999)
+f <- readLines("women10Mile_2001")
+head(f)
 
 ##(1) one file
 
