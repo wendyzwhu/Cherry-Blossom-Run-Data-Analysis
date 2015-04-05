db74d2e7c9d081e0b72354b16d043acc09e113f9
Ricebabyhaha <zwhu@ucdavis.edu>
2015-04-04 17:03:12 -0800

    Assignment 1

---
+++ Assignment 1.R
@@ -0,0 +1,12 @@
+##Step one: reading data into R##
+setwd("d:\\00 Davis\\03 2015 Spring Quarter\\Stat 242\\Assignment 1\\stat242_2015\\Assignment1\\data")
+list.dirs() #check the current directory
+dir()
+list.files()
+readLines("men10Mile_1999")
+class(men10Mile_1999)
+
+##(1) one file
+
+##(2) multiple files
+##Step two: EDA##
