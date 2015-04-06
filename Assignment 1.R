2ec2307ca589ffbbfdc7c6f8698e63a7296c8f92
Ricebabyhaha <zwhu@ucdavis.edu>
2015-04-05 20:43:38 -0800

    Assignment 1

--- Assignment 1.R
+++ Assignment 1.R
@@ -11,19 +11,26 @@ head(f)
 ##(1) one file
 #1999
 ff <- ff[-(1:6)]
-ff <- read.fwf("women10Mile_1999", widths = c( 5,9,21,2,18,7,5))
+ff <- read.fwf("women10Mile_1999", lapply(strsplit(ff[8], " "), count))
 ff[7:13]
 ff[7]
 nchar(ff[7])
 nchar(strsplit(ff[8], " "))
-lapply(strsplit(ff[8], " "), nchar)
+
+lapply(strsplit(ff[8], " "), count )
+count = function (x) {
+  #Add 1 to each width#
+  #there is a blank after each cloumn#
+  nchar(x) +1
+}
 strsplit(ff[8]," ")
 
 ##(2) multiple files
 ##Step two: EDA##
 ##(3) Function for 1999
 ###Top function
-readCherryBlossom = function (filename, txt = readLines(filename)) {
+
+readCherryBlossom = function (filename, txt = readLines(filename, warn = FALSE)) {
  
   header = splitHeader(txt)[1]
   header
