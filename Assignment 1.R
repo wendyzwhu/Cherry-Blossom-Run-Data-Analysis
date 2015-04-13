8970f5afd91fe0ca0bd77cd30ae47d93a00c79f6
Ricebabyhaha <zwhu@ucdavis.edu>
2015-04-12 23:38:35 -0800

    Assignment 1

--- Assignment 1.R
+++ Assignment 1.R
@@ -263,7 +263,43 @@ hist(winnerAge, main = "Distribution of Winner Age (1999-2010)", xlab = "Winner 
 
 ###time 
 Wtime <- function(x) {select(rankOne[[x]],matches("^time|^net|^ net tim\\s*$"))}
-winnerTime <- sapply(c(1:24), Wtime)
-timeConvert <- sapply(c(1:24), Age)
+winnerTime <- lapply(c(1:24), Wtime)
+timeConvert <- function(x){period_to_seconds(ms(unlist(winnerTime[[x]])))}
 winnerTime[[12]] <- select(rankOne[[12]], matches("^time"))
+winnerSecond <- as.numeric(sapply(c(1:24), timeConvert))
+year <- as.numeric(1999:2010)
+plot(winnerSecond[1:12],type = "o", axes = FALSE, pch = 22, lty = 2, ylim = range(winnerSecond), ylab = 'Time', xlab = "Year")
+lines(winnerSecond[13:24],type = "o", pch =22, col = 'red')
+title(main = "Winner Time", font.main = 4)
+axis(1, at = 1:12, lab = as.character(c(1999:2010)))
+axis(2,  las = 1, at = c(2500,2600,2700,2800,2900,3000,3100,3200,3300,3400,3500,3600))
+legend("top", xjust = 0,c("Men","Women") , cex=0.8,col=c("blue","red"), pch = 21:22, lty = 1:2, bty = "n")
 
+##Average Time??
+timeConvertPlus <- function(timeframe,x) {
+  ifelse(length(strsplit(as.character(unlist(timeframe[1,x],":"))))==3,
+                period_to_seconds(hms(unlist(timeframe[1,x))),
+                period_to_seconds(ms(unlist(timeframe[1,x]))))}
+
+alltime <- lapply(c(1:24), function(x) {select(cherryRun[[x]],matches("^time|^net|^ net tim\\s*$"))})
+alltime[[12]]<- select(cherryRun[[12]], matches("^time"))
+
+##Hometown
+Homeall <- function(x) {select(cherryRun[[x]],contains("hometown"))}
+allHome <- as.character(unlist(sapply(c(1:24), Homeall)))
+
+Kenyaall <- sum(str_count(winnerHome, "Kenya\\s*"))/113288
+Kenall <- sum(str_count(winnerHome,"[K[Ee][Nn]\\s]*"))/113288
+Ethiopia<- sum(str_count(winnerHome, winnerHome[1]))/113288
+Morocco <- sum(str_count(winnerHome, winnerHome[10]))/113288
+Others <- (113288 - kenya - ken - ethiopia - morocco)/113288
+pie(c(Kenya,Ken,Ethiopia,Morocco,Others),labels = c("Kenya","Ken","Ethiopia","Morocco","Other"), col = brewer.pal(5,"Purples"), main = "Hometown of Winners (1999 - 2010)")
+
+##Age
+Ageall <- function(x) {select(cherryRun[[x]],contains("ag"))}
+allAge <- as.numeric(unlist(sapply(c(1:24), Ageall)))
+names(allAge) <- NULL
+hist(allAge, main = "Distribution of Age (1999-2010)", xlab = "Age", col = heat.colors(5))
+
+##gender
+runner <- function (x)
